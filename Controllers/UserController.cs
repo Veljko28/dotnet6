42a645c58180543b1a7b7a19b12b941dcc98fe66
Veljko28 <charlibear28@gmail.com>
2022-05-17 19:17:04 +0200

    adding the template and the creation of the user controller

---
+++ Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ReRun6.Controllers
+{
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+
+        public UserController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [HttpGet]
+        public IActionResult GetUserById([FromRoute] string id)
+        {
+            return Ok();
+        }
+    }
+}
