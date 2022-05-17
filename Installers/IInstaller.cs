07169d2497291edb675eabcf366b0d5615b7546b
Veljko28 <charlibear28@gmail.com>
2022-05-17 19:17:56 +0200

    trying to setup installers in dot net 6

---
+++ Installers/IInstaller.cs
@@ -0,0 +1,8 @@
+﻿namespace ReRun6.Installers
+{
+    public interface IInstaller
+    {
+        void InstallServices(IConfiguration configuration, IServiceCollection services);
+
+    }
+}
---
+++ Installers/InstallerExstentions.cs
@@ -0,0 +1,16 @@
+﻿namespace ReRun6.Installers
+{
+    public static class InstallerExstentions
+    {
+		public static void InstallServicesInAssembly(this IServiceCollection services, IConfiguration Configuration)
+		{
+			var installers = typeof(Program).Assembly.ExportedTypes.Where(x => typeof(IInstaller).IsAssignableFrom(x) && !x.IsInterface && !x.IsAbstract)
+					  .Select(Activator.CreateInstance).Cast<IInstaller>().ToList();
+
+			foreach (var installer in installers)
+			{
+				installer.InstallServices(Configuration, services);
+			}
+		}
+	}
+}
