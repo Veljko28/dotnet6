b67a19a3fc50e02ad17cdecc9d7442593fdb5e82
Veljko28 <charlibear28@gmail.com>
2022-05-17 19:17:18 +0200

    adding api routes to the helper folder

---
+++ Helpers/ApiRoutes.cs
@@ -0,0 +1,14 @@
+﻿namespace ReRun6.Helpers
+{
+    public static class ApiRoutes
+    {
+        public const string root = "/api";
+        public const string version = "/v1";
+       static class UserRoutes
+       {
+            public const string parent = "/user";
+            public const string GetUserById = root + version + parent + "/{id}";
+       }
+
+    }
+}
