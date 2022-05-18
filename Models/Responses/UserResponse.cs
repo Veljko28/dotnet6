1df6df9aad95c3f7eb421dd0591b7c1913822fea
Veljko28 <charlibear28@gmail.com>
2022-05-18 09:55:32 +0200

    Adding user models general and response

---
+++ Models/Responses/UserResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace ReRun6.Models.Responses
+{
+    public class UserResponse
+    {
+        public string Id { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public int Points { get; set; }
+    }
+}
---
+++ Models/UserModel.cs
@@ -0,0 +1,11 @@
+﻿namespace ReRun6.Models
+{
+    public class UserModel
+    {
+        public string Id { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public int Points { get; set; } = 0;
+    }
+}
