5f5bc07231897895db5850d0193c3be391da0dc7
Veljko28 <charlibear28@gmail.com>
2022-05-18 09:56:01 +0200

    Adjusting program file for using installers

--- Program.cs
+++ Program.cs
@@ -4,11 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.InstallServicesInAssembly();
+builder.Services.InstallServicesInAssembly(builder.Configuration);
 
 var app = builder.Build();
 
