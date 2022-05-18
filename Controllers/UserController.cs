bc762929a2e87529f1acefd62274b72e0cba5b01
Veljko28 <charlibear28@gmail.com>
2022-05-18 09:53:59 +0200

    Adding new routes to the user controller

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,4 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ReRun6.Helpers;
+using ReRun6.Models.Requests;
+using ReRun6.Models.Responses;
+using ReRun6.Repository.Interfaces;
 
 namespace ReRun6.Controllers
 {
@@ -6,16 +10,37 @@ namespace ReRun6.Controllers
     public class UserController : ControllerBase
     {
         private readonly IConfiguration _config;
+        private readonly IUserRepository _userRepository;
 
-        public UserController(IConfiguration config)
+        public UserController(IConfiguration config, IUserRepository userRepository)
         {
             _config = config;
+            _userRepository = userRepository;
         }
 
-        [HttpGet]
-        public IActionResult GetUserById([FromRoute] string id)
+        [HttpGet(ApiRoutes.UserRoutes.GetUserById)]
+        public async Task<IActionResult> GetUserById([FromRoute] string id)
         {
-            return Ok();
+            UserResponse user = await _userRepository.GetUserByIdAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return Ok(user);
+        }
+
+        [HttpPost(ApiRoutes.UserRoutes.Register)]
+        public async Task<IActionResult> Register([FromBody] RegisterRequest req)
+        {
+            try
+            {
+                UserResponse user = await _userRepository.RegisterUserAsync(req);
+                return Ok(user);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
     }
 }
