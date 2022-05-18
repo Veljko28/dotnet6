90185e428d7f59190edfe9824d3378a6907fa521
Veljko28 <charlibear28@gmail.com>
2022-05-18 09:55:49 +0200

    Adding the needed files for the user repository

---
+++ Repository/Interfaces/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using ReRun6.Models.Requests;
+using ReRun6.Models.Responses;
+
+namespace ReRun6.Repository.Interfaces
+{
+    public interface IUserRepository
+    {
+        public Task<UserResponse> GetUserByIdAsync(string id);
+
+        public Task<UserResponse> RegisterUserAsync(RegisterRequest req);
+    }
+}
