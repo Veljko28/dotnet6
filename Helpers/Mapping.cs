33bf0801baa25fce5fd7a61cf0115eefc75581c3
Veljko28 <charlibear28@gmail.com>
2022-05-19 12:04:54 +0200

    Adding a custom class for mapping different models of the same type

---
+++ Helpers/Mapping.cs
@@ -0,0 +1,20 @@
+﻿using ReRun6.Models;
+using ReRun6.Models.Responses;
+
+namespace ReRun6.Helpers
+{
+    public static class Mapping
+    {
+        public static UserResponse UserModelToResponse(UserModel user)
+        {
+            return new UserResponse()
+            {
+                Id = user.Id,
+                UserName = user.UserName,
+                Email = user.Email,
+                Points = user.Points
+            };
+        }
+
+    }
+}
