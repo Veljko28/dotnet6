9864cfc1d5f5e5cfe1e8033369a737e3206e4e82
Veljko28 <charlibear28@gmail.com>
2022-05-21 15:32:02 +0200

    Half implemented sql user repository

---
+++ ReRun6/Repository/SqlUserRepository.cs
@@ -0,0 +1,53 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using ReRun6.Helpers;
+using ReRun6.Models;
+using ReRun6.Models.Requests;
+using ReRun6.Models.Responses;
+using ReRun6.Repository.Interfaces;
+using System.Data;
+
+namespace ReRun6.Repository
+{
+    public class SqlUserRepository : IUserRepository
+    {
+        private readonly ISqlDataAccess _db;
+
+        public SqlUserRepository(ISqlDataAccess db)
+        {
+            _db = db;
+        }
+
+        public async Task<List<UserModel>> GetAllUsers()
+        {
+            return (await _db.LoadData<UserModel, dynamic>("dbo.spGetAllUsers", new { })).ToList();
+
+        }
+
+        public async Task<UserResponse> GetUserByIdAsync(string id)
+        {
+            UserModel user = (await _db.LoadData<UserModel, dynamic>("dbo.spUserById", new { })).FirstOrDefault();
+            if (user == null)
+            {
+                throw new Exception("This user can't be found");
+            }
+            return Mapping.UserModelToResponse(user);
+
+        }
+
+        public bool GivePoints(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UserResponse> LoginUserAsync(LoginRequest req)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UserResponse> RegisterUserAsync(RegisterRequest req)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
