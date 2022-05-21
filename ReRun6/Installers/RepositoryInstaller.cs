4498e5abd7d1ee38c6a3a7b42584e351af1b593f
Veljko28 <charlibear28@gmail.com>
2022-05-21 15:31:44 +0200

    Adding a singleton to installer for data access

---
+++ ReRun6/Installers/RepositoryInstaller.cs
@@ -0,0 +1,16 @@
+﻿using ReRun6.Helpers;
+using ReRun6.Repository;
+using ReRun6.Repository.Interfaces;
+
+namespace ReRun6.Installers
+{
+    public class RepositoryInstaller : IInstaller
+    {
+        public void InstallServices(IConfiguration configuration, IServiceCollection services)
+        {
+            services.AddScoped<IUserRepository, UserRepository>();
+
+            services.AddSingleton<ISqlDataAccess, SqlDataAccess>();
+        }
+    }
+}
