046588afe7a55bbb36b376500c7fafead08c0143
Veljko28 <charlibear28@gmail.com>
2022-05-22 16:17:07 +0200

    adding a stored procedure for adding users

---
+++ PointsDb/dbo/Stored Procedures/spAddUser.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE [dbo].[spAddUser]
+	@UserName NVARCHAR(150) NOT NULL,
+	@Email NVARCHAR(100) NOT NULL,
+	@Password NVARCHAR(256) NOT NULL,
+	@Points INT NOT NULL
+AS
+begin
+	INSERT INTO [dbo].[Users] VALUES (@UserName, @Email, @Password, @Points);
+end
