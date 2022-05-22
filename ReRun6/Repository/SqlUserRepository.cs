25afe0aba83fa4674b781d9c983c5c1535513b76
Veljko28 <charlibear28@gmail.com>
2022-05-22 16:16:30 +0200

    Implementing Register and Login in sql user repo

--- ReRun6/Repository/SqlUserRepository.cs
+++ ReRun6/Repository/SqlUserRepository.cs
@@ -35,19 +35,46 @@ namespace ReRun6.Repository
 
         }
 
-        public bool GivePoints(string id)
+        public async Task<bool> GivePoints(string id)
         {
-            throw new NotImplementedException();
+            return await _db.SaveData<dynamic>("dbo.givePoints", new { userId = id });
         }
 
-        public Task<UserResponse> LoginUserAsync(LoginRequest req)
+        public async Task<UserResponse> LoginUserAsync(LoginRequest req)
         {
-            throw new NotImplementedException();
+            UserModel user = (await _db.LoadData<UserModel, dynamic>("dbo.spFindUser", new { req.UserName })).FirstOrDefault();
+            if (user == null || !PasswordHashing.ComparePasswords(user.Password, req.Password))
+            {
+                throw new Exception("Cannot match the username and/or password to any user !");
+            }
+            return Mapping.UserModelToResponse(user);
         }
 
-        public Task<UserResponse> RegisterUserAsync(RegisterRequest req)
+        public async Task<UserResponse> RegisterUserAsync(RegisterRequest req)
         {
-            throw new NotImplementedException();
+            if ((req.ConfirmPassword != req.Password) || req.Password == "")
+            {
+                throw new Exception("The passwords don't match!");
+            }
+
+            List<UserModel> users = (await _db.LoadData<UserModel, dynamic>("dbo.GetAllUsers", new { })).ToList();
+
+
+            if (users.Find(x => x.UserName == req.UserName) != null || req.UserName == "")
+            {
+                throw new Exception("The username is already taken!");
+            }
+            if (users.Find(x => x.Email == req.Email) != null || req.Email == "")
+            {
+                throw new Exception("The email is already in use!");
+            }
+
+            UserModel user = Mapping.RegisterRequestToUserModel(req);
+            await _db.SaveData<dynamic>("dbo.spAddUser", 
+                new { user.UserName, user.Email, 
+                    Password = PasswordHashing.HashPassword(user.Password), Points = 0 });
+
+            return Mapping.UserModelToResponse(user);
         }
     }
 }
