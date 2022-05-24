d45d19df07f9e7fdb9660802cd273b7f1691dc8a
Veljko28 <charlibear28@gmail.com>
2022-05-24 23:11:33 +0200

    creating the product request model

---
+++ ReRun6/Models/Requests/ProductRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace ReRun6.Models.Requests
+{
+    public class ProductRequest
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public int Cost { get; set; }
+        public int Amount { get; set; }
+    }
+}
