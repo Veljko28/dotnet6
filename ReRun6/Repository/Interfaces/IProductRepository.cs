38dc2a1ae193f67767b2bd01c879cf1a3136aa2e
Veljko28 <charlibear28@gmail.com>
2022-05-24 23:12:17 +0200

    creating the repo interface and implementation boilerplate

---
+++ ReRun6/Repository/Interfaces/IProductRepository.cs
@@ -0,0 +1,11 @@
+﻿using ReRun6.Models.Requests;
+
+namespace ReRun6.Repository.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<bool> AddProductAsync(ProductRequest req);
+        Task<bool> TakeProductAsync(string id);
+        Task<bool> DeleteProductAsync(string id);
+    }
+}
---
+++ ReRun6/Repository/ProductRepository.cs
@@ -0,0 +1,23 @@
+﻿using ReRun6.Models.Requests;
+using ReRun6.Repository.Interfaces;
+
+namespace ReRun6.Repository
+{
+    public class ProductRepository : IProductRepository
+    {
+        public Task<bool> AddProductAsync(ProductRequest req)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteProductAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> TakeProductAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
