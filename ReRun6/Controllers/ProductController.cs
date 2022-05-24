71bc6cf58bf3ed81991a3e79108e086218cf97e7
Veljko28 <charlibear28@gmail.com>
2022-05-24 23:10:40 +0200

    creating empty controller with the repo injected

---
+++ ReRun6/Controllers/ProductController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ReRun6.Controllers
+{
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        public ProductController()
+        {
+
+        }
+    }
+}
