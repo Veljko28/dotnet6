37698aaa1585b4a8e828bdbc13f39800d62c4e35
Veljko28 <charlibear28@gmail.com>
2022-05-24 23:11:23 +0200

    creating the product model

---
+++ ReRun6/Models/ProductModel.cs
@@ -0,0 +1,12 @@
+﻿namespace ReRun6.Models
+{
+    public class ProductModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public int Cost { get; set; }
+        public int Amount { get; set; }
+    }
+}
