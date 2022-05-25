b9cb248efd4e40fab426ab3d9c891161f3b1f95f
Veljko28 <charlibear28@gmail.com>
2022-05-25 17:38:59 +0200

    implementing all the methods in product repository

--- ReRun6/Repository/ProductRepository.cs
+++ ReRun6/Repository/ProductRepository.cs
@@ -1,23 +1,45 @@
-﻿using ReRun6.Models.Requests;
+﻿using ReRun6.Helpers;
+using ReRun6.Models;
+using ReRun6.Models.Requests;
 using ReRun6.Repository.Interfaces;
 
 namespace ReRun6.Repository
 {
     public class ProductRepository : IProductRepository
     {
-        public Task<bool> AddProductAsync(ProductRequest req)
+        private readonly ISqlDataAccess _db;
+
+        public ProductRepository(ISqlDataAccess db)
         {
-            throw new NotImplementedException();
+            _db = db;
+        }
+        public async Task<bool> AddProductAsync(ProductRequest req)
+        {
+            return await _db.SaveData<dynamic>("dbo.spAddProduct", new { req.Name, req.Image, req.Amount, req.Cost, req.Description });
         }
 
-        public Task<bool> DeleteProductAsync(string id)
+        public async Task<bool> DeleteProductAsync(string id)
         {
-            throw new NotImplementedException();
+            return await _db.SaveData<dynamic>("dbo.spDeleteProduct", new { productId = id });
         }
 
-        public Task<bool> TakeProductAsync(string id)
+        public async Task<IEnumerable<ProductModel>> GetAllProductsAsync()
         {
-            throw new NotImplementedException();
+            return await _db.LoadData<ProductModel,dynamic>("dbo.spGetAllProducts", new { });
+        }
+
+        public async Task<bool> TakeProductAsync(string id)
+        {
+            ProductModel product = (await _db.LoadData<ProductModel,dynamic>("dbo.spTakeProduct", new {productId = id})).FirstOrDefault();
+            if (product == null)
+            {
+                throw new Exception("Cannot find a product with this id !");
+            }
+            if (product.Amount > 0)
+            {
+                return await _db.SaveData<dynamic>("dbo.spTakeProduct", new { productId = id });
+            }
+            else throw new Exception("There is no more products of this type left !");
         }
     }
 }
