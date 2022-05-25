ce87ac51fb5b48b79f66618e052849d56adfd1b8
Veljko28 <charlibear28@gmail.com>
2022-05-25 17:38:44 +0200

    Adding get all products to interface

--- ReRun6/Repository/Interfaces/IProductRepository.cs
+++ ReRun6/Repository/Interfaces/IProductRepository.cs
@@ -1,9 +1,11 @@
-﻿using ReRun6.Models.Requests;
+﻿using ReRun6.Models;
+using ReRun6.Models.Requests;
 
 namespace ReRun6.Repository.Interfaces
 {
     public interface IProductRepository
     {
+        Task<IEnumerable<ProductModel>> GetAllProductsAsync();
         Task<bool> AddProductAsync(ProductRequest req);
         Task<bool> TakeProductAsync(string id);
         Task<bool> DeleteProductAsync(string id);
