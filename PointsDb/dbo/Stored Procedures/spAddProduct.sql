a9642f56dff0f7273b0dac3b52d7a0f9fc7d1b98
Veljko28 <charlibear28@gmail.com>
2022-05-25 17:37:57 +0200

    stored procedure for adding products

---
+++ PointsDb/dbo/Stored Procedures/spAddProduct.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [dbo].[spAddProduct]
+	@Name NVARCHAR(100),
+	@Description NVARCHAR(256),
+	@Image NVARCHAR(256),
+	@Cost MONEY,
+	@Amount INT
+AS
+begin
+	INSERT INTO [dbo].[Products] VALUES (@Name,@Description,@Image,@Cost,@Amount);
+end
