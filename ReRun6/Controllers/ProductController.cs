3b4aad89836b9cfeb3a8503ec979c6e0884f282d
Veljko28 <charlibear28@gmail.com>
2022-05-26 18:35:36 +0200

    Making endpoints for product controller

--- ReRun6/Controllers/ProductController.cs
+++ ReRun6/Controllers/ProductController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ReRun6.Helpers;
+using ReRun6.Models.Requests;
 using ReRun6.Repository.Interfaces;
 
 namespace ReRun6.Controllers
@@ -11,6 +13,26 @@ namespace ReRun6.Controllers
         public ProductController(IProductRepository products)
         {
             _products = products;
+        }
+
+        [HttpGet(ApiRoutes.ProductRoutes.GetAllProducts)]
+        public async Task<IActionResult> GetAllProducts()
+        {
+            return Ok(await _products.GetAllProductsAsync());
+        }
+
+        [HttpPost(ApiRoutes.ProductRoutes.AddProduct)]
+        public async Task<IActionResult> AddProduct([FromBody] ProductRequest req)
+        {
+            bool ok = await _products.AddProductAsync(req);
+            return Ok(ok);
+        }
+
+        [HttpPost(ApiRoutes.ProductRoutes.TakeProduct)]
+        public async Task<IActionResult> TakeProduct([FromRoute] string id)
+        {
+            bool ok = await _products.TakeProductAsync(id);
+            return Ok(ok);
         }
     }
 }
