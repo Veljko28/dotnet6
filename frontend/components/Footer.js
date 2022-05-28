99f8d8a89193f8b31da0f78e98233ad72f63986b
Veljko28 <charlibear28@gmail.com>
2022-05-28 17:47:28 +0200

    start of creating a footer

---
+++ frontend/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+        Points Project &copy; Copyright 2022 - Terms of service
+    </div>
+  )
+}
+
+export default Footer
