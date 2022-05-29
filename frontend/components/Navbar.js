74763ee346c564485f7f0499db0590b2e6336a53
Veljko28 <charlibear28@gmail.com>
2022-05-29 17:21:45 +0200

    updating navbar and adding its styles

--- frontend/components/Navbar.js
+++ frontend/components/Navbar.js
@@ -6,20 +6,20 @@ import { Grid } from '@mui/material';
 const Navbar = () => {
   return (
     <nav>
-        <Grid container style={{paddingTop: 10}}>
-            <Grid item xs={6} style={{padding: 10, paddingLeft: 0}}>
-                    <span style={{padding: 10}}>
+        <Grid container style={{paddingTop: 10, backgroundColor: '#fff', borderBottom: '2px solid #999'}}>
+            <Grid item xs={6} style={{padding: 15}}>
+                    <span style={{padding: 10}} className="nav-link">
                         <Link href="/">Home</Link>
                     </span>
-                    <span style={{padding: 10}}>
+                    <span style={{padding: 10}} className="nav-link">
                         <Link href="/register">Register</Link>  
                     </span>
-                    <span style={{padding: 10}}>
+                    <span style={{padding: 10}} className="nav-link">
                         <Link href="/login">Login</Link>
                     </span>
                 </Grid>
             <Grid item xs={4} >
-                    <Image src="/logo.png" width={75} height={45} />
+                    <Image src="/logo.png" width={85} height={45} />
             </Grid>
             <Grid item xs={2}/>
         </Grid>
