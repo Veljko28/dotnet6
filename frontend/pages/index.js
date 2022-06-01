ccf16019d9dad266f2d56db3ed916b86f4abef8c
Veljko28 <charlibear28@gmail.com>
2022-06-01 15:33:53 +0200

    displaying a single card on home page

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,11 +1,13 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import ProductCard from '../components/ProductCard'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      Home
+      <ProductCard id={1} image={""} name="test1" price={10500} 
+      desc={"This is a test product. It has a really long descrption just for testing purposes."}/>
     </div>
   )
 }
