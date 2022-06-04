02be3e0681be76be54b6f409959ede2ec9f6e26a
Veljko28 <charlibear28@gmail.com>
2022-06-04 15:57:45 +0200

    adding intro block for home page

---
+++ Frontend/components/Home/IntroBlock.js
@@ -0,0 +1,23 @@
+import { Grid, Typography } from '@mui/material'
+import React from 'react'
+import Image from 'next/image';
+
+const IntroBlock = () => {
+  return (
+    <Grid container className="intro-container">
+        <Grid xs={2} item/>
+        <Grid xs={8} item container>
+            <Grid md={6} xs={12} item style={{textAlign: "center",marginTop: 50}}>
+                <Typography variant="h3" style={{fontWeight: 700}}>Earn points &amp; get worthy prizes!</Typography>
+                <Typography variant="h5"  style={{fontWeight: 100}}>Million points already earned <br/>and 10,000+ prizes given away</Typography>
+            </Grid>            
+            <Grid md={6} xs={12} item>
+                <Image src="/gift-cards.jpg" width={550} height={350} style={{borderRadius: 5}}/>
+            </Grid>            
+        </Grid>
+        <Grid xs={2} item/>
+    </Grid>
+  )
+}
+
+export default IntroBlock
