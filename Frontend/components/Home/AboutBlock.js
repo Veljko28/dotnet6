5df877cc73aac32df159b691bff9a4133c1dc398
Veljko28 <charlibear28@gmail.com>
2022-06-05 13:16:42 +0200

    small changes to home blocks

--- Frontend/components/Home/AboutBlock.js
+++ Frontend/components/Home/AboutBlock.js
@@ -8,11 +8,13 @@ const AboutBlock = () => {
         <Grid xs={2} item/>
         <Grid xs={8} item container>
             <div>
-                <Typography variant="h2" style={{fontWeight: 700, borderTop: "3px solid #0cafe5"}}>About</Typography>
+                <Typography variant="h3" style={{fontWeight: 700, borderTop: "3px solid #0cafe5"}}>About</Typography>
             </div>
             <div>
                 <Typography variant="h6"  style={{fontWeight: 100}}>This website is made for people who want to get a quick buck 
                 doing simple jobs/tasks and earning points. With those points they can withdraw gift cards, items and money to their credit card or Paypal.
+                <br/>
+                <br/>
                 We always update our prizes and rewards. If you want us to add some item, or have a problem with your job/task contant our support team.</Typography>
             </div>
         </Grid>
--- Frontend/components/Home/IntroBlock.js
+++ Frontend/components/Home/IntroBlock.js
@@ -1,4 +1,4 @@
-import { Grid, Typography } from '@mui/material'
+import { Grid, Typography, Button } from '@mui/material'
 import React from 'react'
 import Image from 'next/image';
 
@@ -10,6 +10,7 @@ const IntroBlock = () => {
             <Grid md={6} xs={12} item style={{textAlign: "center",marginTop: 50}}>
                 <Typography variant="h3" style={{fontWeight: 700}}>Earn points &amp; get worthy prizes!</Typography>
                 <Typography variant="h5"  style={{fontWeight: 100}}>Million points already earned <br/>and 10,000+ prizes given away</Typography>
+                <Button variant="contained" style={{backgroundColor: "white", color: "#0cafe5", marginTop: 25}}>Start earning</Button>
             </Grid>            
             <Grid md={6} xs={12} item>
                 <Image src="/gift-cards.jpg" width={550} height={350} style={{borderRadius: 5}}/>
