38cb221c4202438aacc5497bca2744952f771c3a
Veljko28 <charlibear28@gmail.com>
2022-06-08 18:07:24 +0200

    updating store procedure and user table

--- PointsDb/dbo/Stored Procedures/spAddUser.sql
+++ PointsDb/dbo/Stored Procedures/spAddUser.sql
@@ -1,9 +1,11 @@
 ﻿CREATE PROCEDURE [dbo].[spAddUser]
+	@Id NVARCHAR(256),
 	@UserName NVARCHAR(150),
 	@Email NVARCHAR(100),
 	@Password NVARCHAR(256),
-	@Points INT
+	@Points INT,
+	@UntilPoints DATETIME
 AS
 begin
-	INSERT INTO [dbo].[Users] VALUES (@UserName, @Email, @Password, @Points);
+	INSERT INTO [dbo].[Users] VALUES (@Id, @UserName, @Email, @Password, @Points, @UntilPoints);
 end
