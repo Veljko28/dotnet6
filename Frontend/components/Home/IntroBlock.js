97a46354a7ca13f422443b8bcc082eac9398cc58
Veljko28 <charlibear28@gmail.com>
2022-06-13 18:41:19 +0200

    intro block get started implementation

--- Frontend/components/Home/IntroBlock.js
+++ Frontend/components/Home/IntroBlock.js
@@ -1,8 +1,21 @@
 import { Grid, Typography, Button } from '@mui/material'
 import React from 'react'
 import Image from 'next/image';
+import Link from 'next/link';
 
 const IntroBlock = () => {
+
+    const [loggedin,handleLoggedIn] = React.useState(false);
+    let jwt=null
+    React.useEffect( () => {
+        if (typeof window !== 'undefined') {
+            jwt = localStorage.getItem('jid');
+            if (jwt != null){
+                handleLoggedIn(true);
+            }
+        }
+    })
+
   return (
     <Grid container className="intro-container">
         <Grid xs={2} item/>
@@ -10,7 +23,9 @@ const IntroBlock = () => {
             <Grid md={6} xs={12} item style={{textAlign: "center",marginTop: 50}}>
                 <Typography variant="h3" style={{fontWeight: 700}}>Earn points &amp; get worthy prizes!</Typography>
                 <Typography variant="h5"  style={{fontWeight: 100}}>Million points already earned <br/>and 10,000+ prizes given away</Typography>
-                <Button variant="contained" style={{backgroundColor: "white", color: "#0cafe5", marginTop: 25}}>Start earning</Button>
+                <Link href={loggedin ? "/jobs/1" : "/login"}>
+                  <Button variant="contained" style={{backgroundColor: "white", color: "#0cafe5", marginTop: 25}}>Start earning</Button>
+                </Link>                
             </Grid>            
             <Grid md={6} xs={12} item>
                 <Image src="/gift-cards.jpg" width={550} height={350} style={{borderRadius: 5}}/>
