430d65ab555b8ef97ff6855d92078bf2c0875290
Veljko28 <charlibear28@gmail.com>
2022-06-15 16:30:32 +0200

    creating the redux store

---
+++ Frontend/redux/store.js
@@ -0,0 +1,12 @@
+import {combineReducers, createStore} from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import userReducer from './reducers/userReducer';
+
+
+const rootReducer = combineReducers({
+    user: userReducer
+});
+
+const store = createStore(rootReducer, composeWithDevTools());
+
+export default store;
